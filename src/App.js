f27bd5aa8bb9b32dae22c2d953a2815dd282b415
rugieri <rugieri.ronaldo@gmail.com>
2021-12-18 21:56:25 -0300

    added header component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Feedback</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
