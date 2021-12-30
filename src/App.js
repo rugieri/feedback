42c24cb80c689f78fbbf4b7d7f48c3d90b641e26
rugieri <rugieri.ronaldo@gmail.com>
2021-12-29 22:54:58 -0300

    added delete feedback feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/Feedback';
+import FeedbackData from './data/FeedbackData';
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData, FeedbackList);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm(' Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
