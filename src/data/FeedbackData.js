9742d33021a8281c26201aaa71177feb3185ee38
rugieri <rugieri.ronaldo@gmail.com>
2022-01-02 12:21:30 -0300

    created a custom button component

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -14,6 +14,12 @@ const FeedbackData = [
     rating: 8,
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
   },
+  {
+    id: 4,
+    rating: 3,
+
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
 ];
 
 export default FeedbackData;
