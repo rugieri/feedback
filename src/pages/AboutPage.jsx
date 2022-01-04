9e7fa845d179c21f602d88483faa9bff4fcb4669
rugieri <rugieri.ronaldo@gmail.com>
2022-01-03 22:51:58 -0300

    created link to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,14 +1,14 @@
 import Card from '../components/shared/Card';
-
+import { Link } from 'react-router-dom';
 function AboutPage() {
   return (
     <Card>
       <div className="about">
-        <h1>About</h1>;
+        <h1>About</h1>
         <p>This is a React App to leave feedbacks for a product or service</p>
         <p>Version 1.0.0</p>
         <p>
-          <a href="/">Back to Home</a>
+          <Link to="/">Back to Home</Link>
         </p>
       </div>
     </Card>
